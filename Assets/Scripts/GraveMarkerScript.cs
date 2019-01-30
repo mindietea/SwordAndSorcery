91dade0225f346e14f0c32a98c1ed32e598cf38e
mindychi <mindychi@gmail.com>
2019-01-30 14:05:45 +0800

    Removed git lfs as it was causing errors

---
+++ Assets/Scripts/GraveMarkerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveMarkerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = transform.position;
+        sphere.transform.localScale = new Vector3(4.0f, 4.0f, 4.0f);
+
+        GameObject monster = SpawnMonster();
+        monster.transform.position += new Vector3(0, 2.0f, 0);
+    }
+
+    GameObject SpawnMonster() {
+        GameObject monster = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        monster.transform.position = transform.position;
+        monster.transform.localScale = new Vector3(4.0f, 4.0f, 4.0f);
+
+        return monster;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
