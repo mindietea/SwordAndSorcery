b49083ee9b8d78d6c5286eb3b6e395c5a257b9f7
Emil Luusua <emilluusua@Emils-MacBook-Pro.local>
2019-02-08 20:44:17 +0800

    Made some fireballs

---
+++ Assets/Scripts/FireProjectileScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectileScript : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform spawnpoint;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Rigidbody fireball;
+            fireball = Instantiate(projectile, spawnpoint.position + Vector3.up,
+                spawnpoint.rotation);
+            fireball.velocity = spawnpoint.TransformDirection(Vector3.forward * speed);
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    public ParticleSystem fireAnimation;
+    private ParticleSystem fireClone;
+    public ParticleSystem explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireClone = Instantiate(fireAnimation, gameObject.transform.position, gameObject.transform.rotation);
+        fireClone.transform.Rotate(0, 270, 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireClone.transform.position = gameObject.transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject collidingObject = collision.gameObject;
+        if(collidingObject.name == "Enemy")
+        {
+            // deal damage to enemy
+        }
+        Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+
+        var main = fireClone.main;
+        main.stopAction = ParticleSystemStopAction.Destroy;
+        fireClone.Clear();
+        fireClone.Stop();
+    }
+}
