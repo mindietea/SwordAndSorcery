49b615f89dddd37ead82658fc314b44085bd95a4
Emil Luusua <emilluusua@gmail.com>
2019-02-10 14:38:47 +0800

    Added damage to procetiles and changed animation

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -4,36 +4,24 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
 {
-    public ParticleSystem fireAnimation;
-    private ParticleSystem fireClone;
+    public ParticleSystem fire;
     public ParticleSystem explosion;
 
     // Start is called before the first frame update
     void Start()
     {
-        fireClone = Instantiate(fireAnimation, gameObject.transform.position, gameObject.transform.rotation);
-        fireClone.transform.Rotate(0, 270, 90);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        fireClone.transform.position = gameObject.transform.position;
+        ParticleSystem fireClone = Instantiate(fire, gameObject.transform.position, gameObject.transform.rotation);
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        GameObject collidingObject = collision.gameObject;
-        if(collidingObject.name == "Enemy")
-        {
-            // deal damage to enemy
-        }
         Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
         Destroy(gameObject);
-
-        var main = fireClone.main;
-        main.stopAction = ParticleSystemStopAction.Destroy;
-        fireClone.Clear();
-        fireClone.Stop();
     }
 }
