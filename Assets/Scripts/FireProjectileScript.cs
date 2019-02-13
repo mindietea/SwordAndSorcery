cd66a0889a789c4a18d70a277f8b9f8c2be595e5
Emil Luusua <emilluusua@gmail.com>
2019-02-13 15:42:52 +0800

    Added a few comments

--- Assets/Scripts/FireProjectileScript.cs
+++ Assets/Scripts/FireProjectileScript.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * Add this script to an object that shall be able to fire projectiles.
+ * The spawnpoint can be specified in the inspector, should probably be set to the gameobject's.
+ */
 public class FireProjectileScript : MonoBehaviour
 {
     public Rigidbody projectile;
@@ -17,10 +20,12 @@ public class FireProjectileScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+        if(Input.GetKeyDown(KeyCode.LeftShift)) // TODO change to button on VR controller
         {
             Rigidbody fireball;
-            fireball = Instantiate(projectile, spawnpoint.position + Vector3.up,
+            Vector3 adjustment = Vector3.up;
+            // Instantiate fireball at the spawnpoint position, adjusted by a Vector3
+            fireball = Instantiate(projectile, spawnpoint.position + adjustment,
                 spawnpoint.rotation);
             fireball.velocity = spawnpoint.TransformDirection(Vector3.forward * speed);
         }
