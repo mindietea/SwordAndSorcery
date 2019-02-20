46c3c49ca3c542706d567f63d803595dee2c5a89
kennedal <kennedal@kth.se>
2019-02-20 10:19:31 +0800

    added parameter spawnTimeRandomness to GraveMarkerScript

--- Assets/Scripts/GraveMarkerScript.cs
+++ Assets/Scripts/GraveMarkerScript.cs
@@ -13,6 +13,9 @@ public class GraveMarkerScript : MonoBehaviour
 	// Wait this many seconds after a spawn until spawning a new enemy
 	public float secondsBetweenSpawns = 4;
 
+	// Randomly add or remove up to this many seconds from secondsBetweenSpawns
+	public float spawnTimeRandomness = 2;
+
     public GameObject monster;
     private bool spawned = false;
 
@@ -28,10 +31,10 @@ public class GraveMarkerScript : MonoBehaviour
 		{
 			GameObject spawn = Instantiate(monster);
             spawn.transform.position = transform.position;
-            spawn.transform.position = new Vector3(spawn.transform.position.x + Random.Range(-6, 6),
+            spawn.transform.position = new Vector3(spawn.transform.position.x + Random.Range(-6f, 6f),
 												   Terrain.activeTerrain.SampleHeight(spawn.transform.position),
-												   spawn.transform.position.z + Random.Range(-6, 6));
-			yield return new WaitForSeconds(secondsBetweenSpawns);
+												   spawn.transform.position.z + Random.Range(-6f, 6f));
+			yield return new WaitForSeconds(secondsBetweenSpawns + Random.Range(-spawnTimeRandomness, spawnTimeRandomness));
 		}
 	}
 
