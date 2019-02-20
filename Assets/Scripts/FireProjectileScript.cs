bac38b14181416d7ab7074251d12adb1635e97e3
Emil Luusua <emilluusua@gmail.com>
2019-02-20 18:00:21 +0800

    Firewand stuff

--- Assets/Scripts/FireProjectileScript.cs
+++ Assets/Scripts/FireProjectileScript.cs
@@ -25,7 +25,7 @@ public class FireProjectileScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Controller.GetHairTriggerDown())
+        if (Controller.GetHairTriggerDown() || Input.GetKeyDown(KeyCode.LeftShift))
         {
             FireProjectile();
         }
