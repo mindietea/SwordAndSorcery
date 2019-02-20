6c5b26f6c05cb91a5407e39b731daed0a4e5019a
kennedal <kennedal@kth.se>
2019-02-20 10:15:05 +0800

    updated GraveMarkerScript to spawn monsters periodically

--- Assets/Scripts/GraveMarkerScript.cs
+++ Assets/Scripts/GraveMarkerScript.cs
@@ -7,8 +7,14 @@ public class GraveMarkerScript : MonoBehaviour
     // Range at which to trigger enemy spawns
     public float range = 7.0f;
 
+	// How many monsters should be spawned
+	public uint monsterCount = 5;
+
+	// Wait this many seconds after a spawn until spawning a new enemy
+	public float secondsBetweenSpawns = 4;
+
     public GameObject monster;
-    public bool spawned = false;
+    private bool spawned = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +22,18 @@ public class GraveMarkerScript : MonoBehaviour
         spawned = false;
     }
 
-    GameObject SpawnMonster() {
-        GameObject monster = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        monster.transform.position = transform.position;
-        monster.transform.localScale = new Vector3(4.0f, 4.0f, 4.0f);
-
-        return monster;
-    }
+	IEnumerator SpawnGroupOfMonsters()
+	{
+		for (int i = 0; i < monsterCount; i++)
+		{
+			GameObject spawn = Instantiate(monster);
+            spawn.transform.position = transform.position;
+            spawn.transform.position = new Vector3(spawn.transform.position.x + Random.Range(-6, 6),
+												   Terrain.activeTerrain.SampleHeight(spawn.transform.position),
+												   spawn.transform.position.z + Random.Range(-6, 6));
+			yield return new WaitForSeconds(secondsBetweenSpawns);
+		}
+	}
 
     // Update is called once per frame
     void Update()
@@ -30,9 +41,7 @@ public class GraveMarkerScript : MonoBehaviour
         if(!spawned && GameManager.GetDistanceToPlayer(gameObject) < range)
         {
             spawned = true;
-            GameObject spawn = Instantiate(monster);
-            spawn.transform.position = transform.position + transform.forward * 2;
-            spawn.transform.position = new Vector3(spawn.transform.position.x, -0.5f, spawn.transform.position.z);
+            StartCoroutine("SpawnGroupOfMonsters");
         }
     }
 }
