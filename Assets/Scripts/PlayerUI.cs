df51a7b1b56afc3fc2240f46a3632912b5aa21af
kennedal <kennedal@kth.se>
2019-02-22 21:59:29 +0800

    hooked up the PlayerUICanvas with the PlayerUI script

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -3,34 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//A basic UI health that display character health dynamically as long as they have a health script as a component and UI canvas
-//How to use: Add this script as character component. A new section should appear to allow you to drag n drop the wanted character and canvas.
+// A basic UI health that display character health dynamically as long as they have a health script as a component and UI canvas
+// How to use: Add this script to the PlayerUICanvas. A new section should appear to allow you to drag n drop the Player prefab,
+// and the GameObjects that display health and enemies killed.
 public class PlayerUI : MonoBehaviour
 {
-    public GameObject actor;
-    public GameObject healthCanvas;
-    public GameObject scoreCanvas;
-    public GameObject remainingCanvas;
+    public GameObject player;
+    public GameObject healthTextObj;
+    public GameObject enemiesKilledTextObj;
 
-    protected HealthScript script;
-    protected Text healthText;
-    protected Text scoreText;
-    protected Text remainingText;
+    private HealthScript healthScript;
+    private Text healthText;
+    private Text enemiesKilledText;
 
     // Start is called before the first frame update
     void Start()
     {
-        script = actor.GetComponentInChildren<HealthScript>() as HealthScript;
-        healthText = healthCanvas.GetComponentInChildren<Text>();
-        scoreText = scoreCanvas.GetComponentInChildren<Text>();
-        remainingText = remainingCanvas.GetComponentInChildren<Text>();
+        healthScript = player.GetComponentInChildren<HealthScript>() as HealthScript;
+        healthText = healthTextObj.GetComponentInChildren<Text>();
+        enemiesKilledText = enemiesKilledTextObj.GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = "Health: " + script.currentHealth.ToString();
-        scoreText.text = "Score: " + GameManager.enemiesKilled.ToString();
-        remainingText.text = "Remaining Enemies: " + GameManager.ENEMIES_TO_KILL.ToString();
+        healthText.text = healthScript.currentHealth.ToString()  + " HP";
+        enemiesKilledText.text = GameManager.enemiesKilled.ToString() + "/" + GameManager.ENEMIES_TO_KILL.ToString() + " Enemies Killed";
     }
 }
