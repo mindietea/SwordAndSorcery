76faea0f4d3699c107a30435822476341512deed
unknown <dabrowski.janek@gmail.com>
2019-02-28 02:19:33 +0800

    Adding pickup script and voiceover

---
+++ Assets/pickupScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupScript : VRTK.VRTK_InteractableObject
+
+{
+
+    private VRTK.VRTK_ControllerEvents controllerEvents;
+    public AudioSource voiceOverAudio;
+    public AudioClip youMakeLaughClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Grabbed(VRTK.VRTK_InteractGrab currentGrabbingObject)
+    {
+
+        Debug.Log("Hello darkness my old friend");
+        base.Grabbed(currentGrabbingObject);
+        voiceOverAudio.Stop();
+        voiceOverAudio.clip = youMakeLaughClip;
+        voiceOverAudio.Play();
+    }
+
+    public override void StartUsing(VRTK.VRTK_InteractUse currentUsingObject)
+    {
+        Debug.Log("Hello darkness my old friend");
+        base.StartUsing(currentUsingObject);
+        voiceOverAudio.Stop();
+        voiceOverAudio.clip = youMakeLaughClip;
+        voiceOverAudio.Play();
+       
+
+    }
+}
