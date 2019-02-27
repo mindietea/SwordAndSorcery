a9f832a51ec96f06a30340fb50c0777f1a0b1805
Mindy Account on public machine for VR <dabrowski.janek@gmail.com>
2019-02-27 20:35:53 +0800

    fix: rabbits explode on the contact

--- Assets/Scripts/RabbitScript.cs
+++ Assets/Scripts/RabbitScript.cs
@@ -38,7 +38,7 @@ public class RabbitScript : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (!anim.GetBool("Dead") && collision.collider.gameObject.tag == "Player")
+        if (!anim.GetBool("Dead") && collision.gameObject.tag == "Player")
         {
             anim.SetBool("Dead", true);
             Instantiate(explosion, gameObject.transform.position + Vector3.up, Quaternion.identity);
